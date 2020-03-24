9dcec321d2a7ca2f6636c89db7f34ea7d9d79817
iammarcusm <marcusmiers@gmail.com>
2020-03-24 16:40:21 -0500

    debugging workbook

--- mpdfollowup/story_content/user.js
+++ mpdfollowup/story_content/user.js
@@ -2,11 +2,14 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "6gqydqiQ0jC":
+      case "6bjgp37LQZ1":
         Script1();
         break;
-      case "6cSw5uHGhnd":
+      case "6KcVTWNbOQC":
         Script2();
+        break;
+      case "5ev8qm9FoV3":
+        Script3();
         break;
   }
 }
@@ -22,6 +25,11 @@ player.SetVar("printUserName",fullName);
 }
 
 function Script2()
+{
+  window.open("print.html","_blank","width=800,height=600,menubar=no,scrollbars=no");
+}
+
+function Script3()
 {
   window.open("print.html","_blank","width=800,height=600,menubar=no,scrollbars=no");
 }

cf2f6c8833b6c8ee84843fc469af653778e80afa
iammarcusm <marcusmiers@gmail.com>
2020-03-24 14:58:19 -0500

    Add files via upload

---
+++ mpdfollowup/process.js
@@ -0,0 +1,28 @@
+//This will get the current date for the user
+var d = new Date(),curr_date = d.getDate(),curr_month = d.getMonth() + 1,curr_year = d.getFullYear();
+var player = opener?opener.top.GetPlayer():{} ;
+/* ------------------ this is ONLY for debugging ----------- */
+if (!player.GetVar) player={
+    GetVar : function(idx) {
+        return {printTitle:"FOLLOW UP PLAN", printUserName:"Unknown"}[idx]
+	}
+}
+/* ------------------ the above is ONLY for debugging ----------- */
+$(function() {
+  if (window.print) {
+    $("#print").text("SAVE").on("click",function() { window.focus(); window.print() })
+    $("#printDiv").show();
+  }
+  var title = player.GetVar("printTitle"), userName = player.GetVar("printUserName");
+  $("#title").html(title);
+  $("#userName").html(userName);
+  $("#date").html(curr_month + "." + curr_date + "." + curr_year);
+  var numberOfAnswers = $(".answer").length;
+  for (var i=1;i<=numberOfAnswers;i++) {
+    var $content = $("#answer"+i);
+    var q = player.GetVar("Q"+i);
+    q=q?q.replace(/(\r\n|\r|\n)/g,"<br/>"):"No response provided."
+    // window.console && console.log(q);
+    $content.append("<p>"+(q)+"</p>");
+  }
+});
---
+++ mpdfollowup/story_content/user.js
@@ -0,0 +1,28 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6ErRMUXJYi2":
+        Script1();
+        break;
+      case "6JqOWiuA3uf":
+        Script2();
+        break;
+  }
+}
+
+function Script1()
+{
+  var lmsAPI = parent
+var name = lmsAPI.GetStudentName();
+var nameArray = name.split(",");
+var fullName = nameArray[1] + " " + nameArray[0];
+var player = GetPlayer();
+player.SetVar("printUserName",fullName);
+}
+
+function Script2()
+{
+  window.open("print.html","_blank","width=800,height=600,menubar=no,scrollbars=no");
+}
+

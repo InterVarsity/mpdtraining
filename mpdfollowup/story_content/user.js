c08137769f04a116d9dabc30bc27a66a2fc43bcf
iammarcusm <marcusmiers@gmail.com>
2020-10-14 10:31:27 -0500

    update

--- mpdfollowup/story_content/user.js
+++ mpdfollowup/story_content/user.js
@@ -2,14 +2,8 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "6NiQD2ik4s3":
+      case "6phdX5nnMAK":
         Script1();
-        break;
-      case "6V1Qhbw1DCF":
-        Script2();
-        break;
-      case "5gYnCA25VGh":
-        Script3();
         break;
   }
 }
@@ -22,15 +16,5 @@ var nameArray = name.split(",");
 var fullName = nameArray[1] + " " + nameArray[0];
 var player = GetPlayer();
 player.SetVar("printUserName",fullName);
-}
-
-function Script2()
-{
-  window.open("print.html","_blank","width=800,height=600,menubar=no,scrollbars=no");
-}
-
-function Script3()
-{
-  window.open("print.html","_blank","width=800,height=600,menubar=no,scrollbars=no");
 }
 
